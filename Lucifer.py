0fd5c7ee6f8a44a37d3a4b83782f416ee4c2ba2c
Ross-the-Moose <89980778+Ross-the-Moose@users.noreply.github.com>
2022-12-19 12:24:02 +0300

    Luci_the_begginning

---
+++ Lucifer.py
@@ -0,0 +1,25 @@
+def replace_b4_start(text):
+    pass
+
+
+def replase_end(text):
+    pass
+
+
+def f_for_des(r, k):
+    r_new = 0
+    return r_new
+
+
+def code(file_name):
+    f = open(file_name, 'rb')
+    while f.tell() > 0:
+        f.read(8)
+    
+    ciphertext = ""
+    return ciphertext
+
+
+def decode(ciphertext):
+    text = ""
+    return text
