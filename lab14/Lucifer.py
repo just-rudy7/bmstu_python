1a9fcd316d15bc47d9a1a49e19127b48d97e5222
Ross-the-Moose <89980778+Ross-the-Moose@users.noreply.github.com>
2022-12-24 01:32:49 +0300

    key_rearrangement_done
    I hope... Should work

--- lab14/Lucifer.py
+++ lab14/Lucifer.py
@@ -3,16 +3,16 @@ from struct import pack as pack
 from struct import unpack as unpack
 from make_a_key import key_changing as shift_key
 
+
 def f_for_des(r, k):
     r_new = 0
     return r_new
 
 
 def code(file_name):
-    f = open(file_name, 'rb')
+    f = open(file_name, 'rb+')
     for i in range(1, 17):
         bin_key = shift_key(i)
-    
     
     ciphertext = ""
     f.close()
@@ -21,3 +21,6 @@ def code(file_name):
 def decode(ciphertext):
     text = ""
     return text
+
+
+code("1.bin")
--- lab14/make_a_key.py
+++ lab14/make_a_key.py
@@ -21,15 +21,26 @@ def reorganize_first_time(bin_key):
         new_bin_key += bin_key[i]
     for i in range(28, 0, -8):
         new_bin_key += bin_key[i]
-
+    
     # print(new_bin_key, len(new_bin_key))
     
     return bin_key
 
 
+# Перестановка со сжатием. Не запоминаю, но передаю в key_change, а потом и в Lucifer
+def choose_48(key):
+    another_str = ""
+    indexes = [14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10, 23, 19, 12, 4, 26, 8, 16, 7, 27, 20, 13, 2, 41, 52, 31, 37,
+               47, 55, 30, 40, 51, 45, 33, 48, 44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32]
+    for i in indexes:
+        another_str += key[i - 1]
+    return another_str
+
+
+# переопределяет ключ по требованию пользователя
 def reset_your_key():
     file = "key.bin"
-    f = open(file, 'wb')
+    f = open(file, 'rb+')
     
     key = input("Введите ключ (0123456789ABCDEF) :<0 для ключа по умолчанию>: ")
     match key:
@@ -52,8 +63,9 @@ def reset_your_key():
     f.close()
 
 
+# сдвиг ключа, запись его в файл, потом перестановка со сжатием до 48 бит, передача результата в Luci
 def key_changing(stage):
-    f = open("key.bin", "rb")
+    f = open("key.bin", "rb+")
     f.seek(16)
     bin_key = (unpack("56s", f.read(56))[0]).decode('utf-8', errors='ignore')
     if stage <= 2 or stage == 9 or stage == 16:
@@ -62,7 +74,11 @@ def key_changing(stage):
         shift = 2
     r_key = bin_key[:28]
     l_key = bin_key[28:]
-    new_key = ""
+    new_key = r_key[-1 * shift:] + r_key[:-1 * shift] + l_key[-1 * shift:] + l_key[:-1 * shift]
+    f.seek(16)
+    f.write(pack("56s", new_key.encode('utf-8')))
+    f.close()
+    new_key = choose_48(new_key)
     return new_key
 
-# key_changing(1)
+# num = key_changing(1)
